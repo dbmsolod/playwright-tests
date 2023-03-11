3467414acdf8bfb623dca43f4defea2accb23eb0
Dmitriy Solodov <dsolodov@avito.ru>
2023-03-11 23:01:15 +0400

    home work 18

---
+++ framework/config.js
@@ -0,0 +1,11 @@
+const config = {
+    credentials: {
+        simpleEmail: 'simpleForm@authenticationtest.com',
+        complexEmail: 'complex@authenticationtest.com',
+        captchaEmail: 'bootstrap@authenticationtest.com',
+        password: 'pa$$w0rd'
+    }
+}
+
+export default config;
+
--- src/login.e2e.spec.js
+++ src/login.e2e.spec.js
@@ -1,30 +1,53 @@
+import config from '../framework/config';
+
 const playwright = require('playwright');
 const chai = require('chai')
 const expect = chai.expect
 
+const BASE_URL = 'https://authenticationtest.com/';
+const someText = 'some text';
+
 let page, browser, context
 
-const selectors = {
-  login: 'input[name=email]',
-  password: 'input[name=password]',
-  loginBtn: 'input[type=submit]',
-  loginSuccess: '.container h1'
+const selectorsAuth = {
+  email: 'input[type="email"]',
+  password: 'input[type="password"]',
+  loginBtn: 'input[class="btn btn-lg btn-primary float-right"]',
+  loginSelect: 'select[id="selectLogin"]',
+  selectOptionYes: 'option[value="yes"]',
+  checkboxLoveManipulation: 'input[id="loveForm"]',
+  loginSuccess: '.container h1',
+  captchaInput: '//input[@id="captcha"]',
+  captchaValue: '//label[@for="captcha"]/code',
+  signInHref: '//span[contains(text(),"Please Sign In")]',
+  signOutHref: '//span/a[contains(text(),"Sign Out")]'
 }
 
-const credentials = {
-  login: 'simpleForm@authenticationtest.com',
-  password: 'pa$$w0rd'
+const selectorsXSS = {
+  searchInput: '//input[@id="search"]',
+  searchBtn: '//input[@value="Search"]',
+  xssText: `//div[@class="card"]/div[@class="card-body"]`
 }
 
-describe('authenticationtest:login', () => {
+async function checkAuthSuccessPage() {
+  const loginSuccessCaption = await page.locator(selectorsAuth.loginSuccess).textContent();
+  expect(loginSuccessCaption).to.equal('Login Success');
+}
+
+async function authEmailAndPassFill(email, password) {
+  await page.locator(selectorsAuth.email).fill(email);
+  await page.locator(selectorsAuth.password).fill(password);
+}
+
+describe('e2e-tests for authenticationtest.com', () => {
   beforeEach(async function() {
     browser = await playwright.chromium.launch({
       headless: false,
-      slowMo: 2000
+      slowMo: 1000
     });
       
     context = await browser.newContext()
-    page = await context.newPage('https://authenticationtest.com/simpleFormAuth/')
+    page = await context.newPage(BASE_URL + 'complexAuth/')
   })
 
   afterEach(async function() {
@@ -32,25 +55,72 @@ describe('authenticationtest:login', () => {
     await browser.close()
   })
 
-  it('should exists', async() => {
-    await page.goto('https://authenticationtest.com/simpleFormAuth');
+  it('Main page exists', async() => {
+    await page.goto(BASE_URL);
     
     const title = await page.title()
     expect(title).to.equal('Authentication Test')
   })
 
-  it('should successfully login and redirect to main page', async() => {
-    await page.goto('https://authenticationtest.com/simpleFormAuth');
 
-    await page.locator(selectors.login).fill(credentials.login);
-    await page.locator(selectors.password).fill(credentials.password);
 
-    await page.click(selectors.loginBtn);
 
+  it.only('Successful authentication with select and checkbox', async() => {
+    await page.goto(BASE_URL + 'complexAuth/');
+
+    await authEmailAndPassFill(config.credentials.complexEmail, config.credentials.password);
+    await page.locator(selectorsAuth.loginSelect).selectOption('yes');
+    await page.locator(selectorsAuth.checkboxLoveManipulation).check();
+
+    await page.click(selectorsAuth.loginBtn);
     await page.waitForLoadState('networkidle');
-    
-    const title = await page.locator(selectors.loginSuccess).textContent();
 
-    expect(title).to.equal('Login Success')
+    await checkAuthSuccessPage();
+  })
+
+
+
+
+
+
+
+  it('Successful interactive authentication with captcha', async() => {
+    await page.goto(BASE_URL + 'bootstrapAuth/');
+
+    await authEmailAndPassFill(credentials.captchaEmail, credentials.password);
+
+    const captchaValue = await page.locator(selectorsAuth.captchaValue).textContent();
+    await page.locator(selectorsAuth.captchaInput).fill(captchaValue);
+
+    await page.click(selectorsAuth.loginBtn);
+    await page.waitForLoadState('networkidle');
+
+    await checkAuthSuccessPage();
+  })
+
+  it('Sign in & Sign Out', async() => {
+    await page.goto(BASE_URL + 'simpleFormAuth/');
+
+    await authEmailAndPassFill(credentials.simpleEmail, credentials.password);
+    await page.click(selectorsAuth.loginBtn);
+    await page.waitForLoadState('networkidle');
+
+    await checkAuthSuccessPage();
+
+    await page.click(selectorsAuth.signOutHref);
+    await page.waitForLoadState('networkidle');
+
+    await page.waitForSelector(selectorsAuth.signInHref);
+  })
+
+  it('Save text in "Recent searches" block on XSS Demo page', async() => {
+    await page.goto(BASE_URL + 'xssDemo/');
+
+    await page.locator(selectorsXSS.searchInput).fill(someText);
+    await page.click(selectorsXSS.searchBtn);
+    await page.waitForLoadState('networkidle');
+
+    const text = await page.locator(selectorsXSS.xssText).textContent();
+    expect(text).to.contain(someText);
   })
 })
